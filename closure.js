02127fc02c4677364074d75a31704ef2da6fec56
Transparenthyf <18098581870@163.com>
2021-11-13 09:23:07 +0800

    closure

--- closure.js
+++ closure.js
@@ -8,12 +8,13 @@ const counter = (() => {
     }
     return num
   }
-// 让匿名函数运行一次，返回一个匿名函数给 add
+// 让函数立即执行一次，返回一个匿名函数给 counter
 })()
 
 const add = '+'
 const sub = '-'
 
+// 闭包之后只能调用 counter 对 num 进行加减
 console.log(counter(add))
 console.log(counter(add))
 console.log(counter(add))
@@ -22,6 +23,5 @@ console.log(counter(add))
 console.log(counter(sub))
 console.log(counter(sub))
 
-// 闭包之后只能调用 counter 进行加减
-
+// 闭包使用完毕，进行内存释放
 counter = null
